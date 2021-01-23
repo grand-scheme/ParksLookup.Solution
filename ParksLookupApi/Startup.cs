f1f46f93f60b0bb429aa21bd2adcc06995b64a40
Shannon Grantski <grantski@pm.me>
2021-01-22 18:52:14 -0800

    add supports; delete valuescontroller

--- ParksLookupApi/Startup.cs
+++ ParksLookupApi/Startup.cs
@@ -6,11 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
 using ParksLookupApi.Models;
 
 namespace ParksLookupApi
