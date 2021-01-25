d04d4ed8cbcc2ecd21644f5e9f812f188351beea
Shannon Grantski <grantski@pm.me>
2021-01-24 21:34:44 -0800

    add Seed data

--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -9,5 +9,36 @@ namespace ParksApi.Models
     {}
 
     public DbSet<Park> Parks {get;set;}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+      .HasData(
+        new Park {
+          ParkId = 1,
+          Name = "Mount Rainier",
+          State = "Washington",
+          StateOrNational = "National"
+        },
+        new Park {
+          ParkId = 2,
+          Name = "North Cascades",
+          State = "Washington",
+          StateOrNational = "National"
+        },
+        new Park {
+          ParkId = 3,
+          Name = "Olympic",
+          State = "Washington",
+          StateOrNational = "National"
+        },
+        new Park {
+          ParkId = 4,
+          Name = "Crater Lake",
+          State = "Oregon",
+          StateOrNational = "National"
+        }
+      );
+    }
   }
 }
