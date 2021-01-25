4a17f1018b6e2343cb02b29f717525dd050d7706
Shannon Grantski <grantski@pm.me>
2021-01-24 23:34:19 -0800

    add controllers for views

--- ParksClient/Controllers/ParksController.cs
+++ ParksClient/Controllers/ParksController.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParksClient.Models;
+
 namespace ParksClient.Controllers
 {
   public class ParksController : Controller 
@@ -6,6 +14,39 @@ namespace ParksClient.Controllers
     {
       var allParks = Park.GetParks();
       return View(allParks);
+    }
+    
+    [HttpPost]
+    public IActionResult Index(Park park)
+      {
+        Park.Post(park);
+        return RedirectToAction("Index");
+      }
+        
+    public IActionResult Details(int id)
+      {
+        var park = Park.GetDetails(id);
+        return View(park);
+      }
+  
+    [HttpPost]
+    public IActionResult Details(int id, Park park)
+    {
+      park.ParkId = id;
+      Park.Put(park);
+      return RedirectToAction("Details", id);
+    }
+  
+    public IActionResult Edit(int id)
+    {
+      var park = Park.GetDetails(id);
+      return View(park);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Park.Delete(id);
+      return RedirectToAction("Index");
     }
   }
 }
