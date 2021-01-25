a12fa6f02c22b8b10c89910a8bf013eedc8aeb1e
Shannon Grantski <grantski@pm.me>
2021-01-24 23:37:29 -0800

    delete unused parameter in Delete

--- ParksClient/Models/ApiHelper.cs
+++ ParksClient/Models/ApiHelper.cs
@@ -39,7 +39,7 @@ namespace ParksClient.Models
       var response = await client.ExecuteTaskAsync(request);
     }
 
-    public static async Task Delete(int id, string deletePark)
+    public static async Task Delete(int id)
     {
       RestClient client = new RestClient("http://localhost:5000/api");
       RestRequest request = new RestRequest($"parks/{id}", Method.DELETE);
