27d7f37081495fe0d63770db9c0782b474b1850f
Shannon Grantski <grantski@pm.me>
2021-01-24 21:54:06 -0800

    make fields required

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace ParksApi.Models
 {
   public class Park
   {
     public int ParkId {get;set;}
+    [Required]
     public string Name {get;set;}
+    [Required]
     public string State {get;set;}
+    [Required]
     public string StateOrNational {get;set;}
   }
 }
