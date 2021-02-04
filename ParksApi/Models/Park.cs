e21054b490bee2f6746a7fa76e7f4bd093cfbd41
Shannon Grantski <grantski@pm.me>
2021-02-04 01:32:48 -0800

    adjust spacing

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -4,10 +4,13 @@ namespace ParksApi.Models
   public class Park
   {
     public int ParkId {get;set;}
+
     [Required]
     public string Name {get;set;}
+
     [Required]
     public string State {get;set;}
+
     [Required]
     public string StateOrNational {get;set;}
   }
